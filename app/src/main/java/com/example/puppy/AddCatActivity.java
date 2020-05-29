dd6c206c58ad5dfbae73bbeb1abad6bef51fafa9
baejinhyun <jinjinjara@kakao.com>
2020-05-29 15:23:19 +0900

    update

---
+++ app/src/main/java/com/example/puppy/AddCatActivity.java
@@ -0,0 +1,13 @@
+package com.example.puppy;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddCatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_cat);
+    }
+}
---
+++ app/src/main/java/com/example/puppy/Cat.java
@@ -0,0 +1,4 @@
+package com.example.puppy;
+
+public class Cat {
+}
